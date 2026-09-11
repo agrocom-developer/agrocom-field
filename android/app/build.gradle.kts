8e0ff25f205d5a5ae1d39215744b5840cb3ca3ac
Miguel Angel Escobar Lazcano <m.escobar_27@outlook.com>
2026-09-11 18:18:08 -0400

    corrige build Android: habilita core library desugaring para flutter_local_notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -56,4 +57,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // flutter_local_notifications (HU-62, avisos locales) exige desugaring habilitado.
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
