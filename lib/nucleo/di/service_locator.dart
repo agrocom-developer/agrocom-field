d57f4910f8cf1c0992c71874697e3eb569ffecb9
Miguel Angel Escobar Lazcano <m.escobar_27@outlook.com>
2026-09-11 16:55:12 -0400

    arranca AvisosLocalesWatcher en main_auxiliar.dart (HU-62)
    Sprint 15 es enteramente del flavor auxiliar (nota del propio sprint: el
    flavor piloto no recibe trabajo nuevo todavía), así que el watcher se
    instancia y arranca ahí, después de configurarDependencias, de vida
    larga y no atado al ciclo de una pantalla. main_piloto.dart no cambia.

--- lib/nucleo/di/service_locator.dart
+++ lib/nucleo/di/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 
+import '../../features/avisos_locales/data/ids_vistos_store.dart';
 import '../../features/ordenes/data/ordenes_repository.dart';
 import '../../features/sesion_vuelo/data/trabajo_repository.dart';
 import '../../features/sesion_vuelo/data/sesion_repository.dart';
@@ -38,6 +39,7 @@ Future<void> configurarDependencias({required Flavor flavor}) async {
   getIt.registerLazySingleton<RolActivoStore>(RolActivoStoreSeguro.new);
   getIt.registerLazySingleton<PreferenciasStore>(PreferenciasStoreLocal.new);
   getIt.registerLazySingleton<NotificadorLocal>(NotificadorLocalPlugin.new);
+  getIt.registerLazySingleton<IdsVistosStore>(IdsVistosStoreLocal.new);
   getIt.registerLazySingleton<ApiClient>(
     () => ApiClient(baseUrl: _apiBaseUrl, tokenStore: getIt<TokenStore>()),
   );
